8b3e4edfce49feedd1e11dd2cf53f8140af3627c
mtl^urass <mtl@glisp.org>
2011-06-05 15:32:49 +0200

    well, fuck

--- load-libs.lisp
+++ load-libs.lisp
@@ -1,16 +1,16 @@
 ;;;Try to get rid of some annoying problems with library loading(it fucks up CGI)
 
 ;;quiet-load: loads a library using quicklisp, suppressing output.
-(defun quiet-load (systems)
-  (let ((*error-output* *standard-output*))
-    (with-output-to-string (*standard-output*)
-      (ql:quickload systems))))
+;(defun quiet-load (systems)
+;  (let ((*error-output* *standard-output*))
+;    (with-output-to-string (*standard-output*)
+;      (ql:quickload systems))))
 
 ;;fast-load: quicklisp has an annoying tendency of downloading a bunch of crap all the time. Try require first to save resources.
-(defun fast-load (system)
-  (handler-case (require system)
-    (sb-int:extension-failure ()
-      (quiet-load (format nil "~a" system)))))
+;(defun fast-load (system)
+; (handler-case (require system)
+;    (sb-int:extension-failure ()
+;      (quiet-load (format nil "~a" system)))))
 
 (when reqs
-  (map 'list #'fast-load reqs))
+  (map 'list #'require reqs))
