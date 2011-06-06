41684b523360713d42bb21c5859d9341276aee04
mtlupurass <mtl@glisp.org>
2011-06-06 04:07:19 -0700

    Fixed some silly bugs

--- load-libs.lisp
+++ load-libs.lisp
@@ -1,14 +1,13 @@
 ;;;Try to get rid of some annoying problems with library loading(it fucks up CGI)
 
-;;fast-load: quicklisp has an annoying tendency of downloading a bunch of crap all the time. Try require first to save resources.
-
 (defparameter *need-install* nil)
 
 (defun fast-load (systems)
   (cond (systems
-	 (handler-case (require system)
+	 (handler-case (require (first systems))
 	   (sb-int:extension-failure ()
-	     (push (format nil "~a" system) *need-install*))))
+	     (push (format nil "~a" (first systems)) *need-install*)))
+         (fast-load (rest systems)))
 	(*need-install*
 	 (load "quicklisp/setup.lisp"))
 	(t t)))
